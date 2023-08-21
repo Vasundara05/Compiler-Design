94cf951a3eb078573564b27d490afe195d19a075
Vasundara05 <113960614+Vasundara05@users.noreply.github.com>
2023-08-21 09:28:26 +0530

    Update exp5.cpp

--- exp5.cpp
+++ exp5.cpp
@@ -1,35 +1,55 @@
-#include <stdio.h>
+// symbol table Implementation
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
 int main()
 {
-    char str[100];//input string with size 100
-
-    int words=0,newline=0,characters=0; // counter variables
-
-    scanf("%[^~]",&str);//scanf formatting    
-
-    for(int i=0;str[i]!='\0';i++)
-     { 
-         if(str[i] == ' ')
-         { 
-              words++;
-         }
-         else if(str[i] == '\n')
-         {
-             newline++;
-              words++;//since with every next line new words start. corner case 1
-         }
-         else if(str[i] != ' ' && str[i] != '\n'){
-         characters++;
-         }
-     }
-    if(characters > 0)//Corner case 2,3.
-    {
-        words++;
-        newline++;
-    }
-     printf("Total ninumber of words : %d\n",words);
-     printf("Total number of les : %d\n",newline);
-     printf("Total number of characters : %d\n",characters);
-    return 0;
+	int x=0, n, i=0,j=0;
+ 	void *mypointer,*a[5];
+ 	char ch,b,Arr2[15],Arr3[15],c;
+ 	printf("Input the expression ending with $ sign:");
+ 	while((c=getchar())!='$')
+ 	{
+  		Arr2[i]=c;
+  		i++;
+ 	}
+ 	n=i-1;
+ 	printf("Given Expression:");
+ 	i=0;
+ 	while(i<=n)
+ 	{
+  		printf("%c",Arr2[i]);
+  		i++;
+ 	}
+ 	printf("\n Symbol Table display\n");
+ 	printf("Symbol \t addr \t type");
+ 	while(j<=n)
+ 	{
+  		c=Arr2[j];
+  		if(isalpha(toascii(c)))
+  		{
+   			mypointer=malloc(c);
+   			a[x]=mypointer;
+   			Arr3[x]=c;
+   			printf("\n%c \t %d \t identifier\n",c,mypointer);
+   			x++;
+   			j++;
+  		}
+  		else
+  		{
+  			ch=c;
+   			if(ch=='+'||ch=='-'||ch=='*'||ch=='=')
+   			{
+    			mypointer=malloc(ch);
+    			a[x]=mypointer;
+    			Arr3[x]=ch;
+    			printf("\n %c \t %d \t operator\n",ch,mypointer);
+    			x++;
+    			j++;
+   			}
+		}
+	}
+   return 0;
 }
-
